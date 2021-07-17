fe4b4b2d0df658924e6429676e872edb2b2edc1f
Erik <erikhau3010@gmail.com>
2021-07-17 09:04:00 +0700

    init

---
+++ 02/main.js
@@ -0,0 +1,26 @@
+const resultContainer = document.querySelector("#result h1");
+const inputCount = document.querySelector("#input-count");
+const randomBtn = document.querySelector("#random-btn");
+
+const lastResult = [];
+
+const random = (min, max) => ~~(Math.random() * (max - min + 1) + min);
+
+randomBtn.addEventListener("click", () => {
+  const maxVal = inputCount.value;
+  const interval = setInterval(() => {
+    resultContainer.innerHTML = random(1, maxVal);
+  }, 10);
+  setTimeout(() => {
+    clearInterval(interval);
+    let expectedResult = random(1, maxVal);
+
+    while (lastResult.includes(expectedResult))
+      expectedResult = random(1, maxVal);
+
+    resultContainer.innerHTML = expectedResult;
+    resultContainer.style.color = "green";
+
+    lastResult.push(expectedResult);
+  }, 3000);
+});
